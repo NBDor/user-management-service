ed6e3850b2f0b7bb64b10fcc02406659007c713b
nbd <nir.bd@ametos.com>
2024-10-22 12:27:11 +0300

    improve(): remove unused 'get_by_email' method

--- app/crud/user.py
+++ app/crud/user.py
@@ -13,8 +13,5 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     def is_superuser(self, user: User) -> bool:
         return user.is_superuser
 
-    def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
-        return db.query(User).filter(User.email == email).first()
-
 
 user = CRUDUser(User)
