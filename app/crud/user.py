dbc02eb6897a8f471d0a460674e13d76c2c0be7e
nbd <nir.bd@ametos.com>
2024-10-23 08:19:48 +0300

    chore(): remove unused imports

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,6 +1,3 @@
-from typing import Optional
-from sqlalchemy.orm import Session
-
 from app.crud.base import CRUDBase
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
